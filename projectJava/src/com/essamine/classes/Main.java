30e8d5188d338e4caa4c8c499c09c4b9d201a013
ibnrochd <essadkaoui01@gmail.com>
2015-12-23 23:07:36 +0000

    tgrdgrgfg

---
+++ projectJava/src/com/essamine/classes/Main.java
@@ -0,0 +1,16 @@
+package com.essamine.classes;
+
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+			Personne personne =new Personne("amine", "ess", 29);
+			System.out.println(personne);
+ 
+	}
+
+}
+ 
